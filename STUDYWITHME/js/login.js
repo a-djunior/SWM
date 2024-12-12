5c640a2be609390287cfdd121946762871c71c57
a-djunior <adaodimasmartinsjunior@gmail.com>
2024-12-12 10:43:47 -0300

    Update login.js

--- STUDYWITHME/js/login.js
+++ STUDYWITHME/js/login.js
@@ -1,23 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Lista de usuários e senhas
-    const users = {
-        'Adão Dimas': { password: 'Adao1234', photo: 'https://github.com/seu_usuario/seu_repositorio/caminho_para_foto_Adao.jpg' },
-        'Maria Silva': { password: 'Maria1234', photo: 'https://github.com/seu_usuario/seu_repositorio/caminho_para_foto_Maria.jpg' },
-        // Adicione os outros usuários aqui com os respectivos links das fotos
-    };
+  // Lista de usuários e senhas
+  const users = {
+    'Adão Dimas': {
+      password: 'Adao1234',
+      photo: 'https://github.com/seu_usuario/seu_repositorio/caminho_para_foto_Adao.jpg'
+    },
+    'Maria Silva': {
+      password: 'Maria1234',
+      photo: 'https://github.com/seu_usuario/seu_repositorio/caminho_para_foto_Maria.jpg'
+    }
+    // Adicione os outros usuários aqui com os respectivos links das fotos
+  };
 
-    // Adicionar evento de envio ao formulário
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+  // Adicionar evento de envio ao formulário
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const usernameField = document.getElementById('username');
+    const passwordField = document.getElementById('password');
+    const username = usernameField.value;
+    const password = passwordField.value;
 
-        // Verificar se o login é bem-sucedido
-        if (users[username] && users[username].password === password) {
-            localStorage.setItem('currentUser', JSON.stringify({ name: username, photo: users[username].photo }));
-            window.location.href = 'inicio.html'; // Redireciona para a página de início
-        } else {
-            alert('Usuário ou senha incorretos. Tente novamente.');
-        }
-    });
+    // Verificar se o login é bem-sucedido
+    if (users[username] && users[username].password === password) {
+      localStorage.setItem('currentUser', JSON.stringify({ name: username, photo: users[username].photo }));
+      window.location.href = 'inicio.html'; // Redireciona para a página de início
+    } else {
+      alert('Usuário ou senha incorretos. Tente novamente.');
+    }
+  });
 });
